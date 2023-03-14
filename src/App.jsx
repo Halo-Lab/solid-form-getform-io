fcde6353afc80c8a1c14b00d8d317889d48b64ce
irinainina <irbris@mail.ru>
2023-03-14 08:32:02 +0200

    feat: add project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,24 @@
-import { createSignal } from "solid-js";
-
-import styles from "./App.module.css";
+import logo from './logo.svg';
+import styles from './App.module.css';
 
 function App() {
-  const [name, setName] = createSignal("");
-  const [email, setEmail] = createSignal("");
-  const [message, setMessage] = createSignal("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log({ name: name(), email: email(), message: message() });
-    setName("");
-    setEmail("");
-    setMessage("");
-  }
-
   return (
-    <form onSubmit={handleSubmit} class={styles.form}>
-      <h2 class={styles.title}>Fill out this form to contact us</h2>
-      <div class={styles.input}>
-        <label htmlFor="name">Your Name</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Your Name"
-          value={name()}
-          onInput={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div class={styles.input}>
-        <label htmlFor="email">Email Address</label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Email Address"
-          value={email()}
-          onInput={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div class={styles.textarea}>
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          placeholder="Message"
-          value={message()}
-          onInput={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <button type="submit" class={styles.button}>
-        Send form
-      </button>
-    </form>
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class={styles.link}
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid
+        </a>
+      </header>
+    </div>
   );
 }
 
